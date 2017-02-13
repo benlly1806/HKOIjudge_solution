3dc401ca9d752755073eef19dcc2d85b7696f930
licorimon <pop2lemon@gmail.com>
2017-02-13 18:53:41 +0800

    Update S150 Enumeration (2014-15 Senior).c

--- S150 Enumeration (2014-15 Senior).c
+++ S150 Enumeration (2014-15 Senior).c
@@ -5,7 +5,7 @@ int main()
     int input,i,j,c;
     scanf("%d",&input);
     for(i=0;i<input;i++)
-	    for(j=0;j<=i;j++)
-            printf("%d%c",j==0?(c=i*i+1):(c-=i),j==i?'\n':' ');
+	for(j=0;j<=i;j++)
+	printf("%d%c",j==0?(c=i*i+1):(c-=i),j==i?'\n':' ');
     return 0;
 }
