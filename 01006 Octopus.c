0f4295fbfa0bff226186fc51c0d0e94cd013c47b
licorimon <pop2lemon@gmail.com>
2017-02-13 18:58:56 +0800

    Add files via upload

---
+++ 01006 Octopus.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<math.h>
+
+int main()
+{
+    int i,n,money,times=0,spend;
+    scanf("%d%d",&money,&n);
+    while(n-->0)
+    {
+        scanf("%d",&spend);
+        money-=spend;
+        if(money<=0&&++times)
+        	money+=250;
+    }
+    printf("$%d\n",250*times);
+    return 0;
+}
