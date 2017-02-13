8a43b8ac8964652cd7122e52559ae944cedff178
licorimon <pop2lemon@gmail.com>
2017-02-13 18:54:47 +0800

    Update J140 Enumeration (2014 Junior).c

--- J140 Enumeration (2014 Junior).c
+++ J140 Enumeration (2014 Junior).c
@@ -2,10 +2,10 @@
 
 int main()
 {
-	int input,i,j,c=1;
+	int input,i,j,count=1;
 	scanf("%d",&input);
 	for(i=1;i<=input;i++)
 		for(j=1;j<=input;j++)
-			printf("%d%c",j==1?c=i*i:(c+=i),j==input?'\n':' ');
+			printf("%d%c",j==1?count=i*i:(count+=i),j==input?'\n':' ');
 	return 0;
 }
