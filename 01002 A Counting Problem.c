c47cba90d80a501507bcf04da57dc57fce55d362
licorimon <pop2lemon@gmail.com>
2017-02-16 00:06:34 +0800

    Update 01002 A Counting Problem.c

--- 01002 A Counting Problem.c
+++ 01002 A Counting Problem.c
@@ -3,19 +3,19 @@
 
 int main ()
 {
-    char sch[1001],fnd[201],fst;
-    int i,j,fdl,x,tmp=0,cnt=0,t=0;
+    char sch [1001] ,fnd [201], fst;
+    int i, j, fdl, x, tmp=0, cnt=0, t=0;
     gets(sch);
     gets(fnd);
-    fdl=strlen(fnd);
-    x=strlen(sch)-fdl+1;
-    for(i=0;i<x;i++,t=0)
+    fdl = strlen(fnd);
+    x = strlen(sch) - fdl + 1;
+    for( i = 0; i < x; i++ ,t = 0 )
     {
-        if(sch[i]==fnd[0])
-            for(j=i;j<i+fdl;j++)
-                if(sch[j]==fnd[t++]);
-        if(t==fdl&&cnt++);
+        if( sch [i] == fnd [0] )
+            for( j = i; j < i + fdl; j++ )
+                if( sch[j] == fnd [t++]);
+        if( t == fdl && cnt++ );
     }
-    printf("%d\n",cnt);
+    printf( "%d\n", cnt );
     return 0;
 }
