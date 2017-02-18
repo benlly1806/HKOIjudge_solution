9a7e8632bd29eadcda3ed20e671a870b7a617d8d
licorimon <pop2lemon@gmail.com>
2017-02-18 23:03:24 +0800

    Update J171 Acronym.c

--- J171 Acronym.c
+++ J171 Acronym.c
@@ -1,31 +1,31 @@
 #include<stdio.h>
 #include<string.h>
 
-int main()
+int main ()
 {
     char acr [101];
     int x;
     scanf("%s\n%d\n", acr, &x);
     int acrlen = strlen(acr), glen, c = 0;
     int i, j;
-    for( i= 0; i < acrlen; i++ )
+    for (i = 0; i < acrlen; i++)
         acr [i] += 32;
     char guess [5001];
-    for( i = 0; i < x; i++, c = 0 )
+    for (i = 0; i < x; i++, c = 0)
     {
-        gets(guess);
-        glen = strlen(guess);
-        for( j = 0; j < glen; j++ )
+        gets (guess);
+        glen = strlen (guess);
+        for (j = 0; j < glen; j++)
         {
-            if( guess [j] >= 'A' && guess [j] <= 'Z' )
+            if (guess [j] >= 'A' && guess [j] <= 'Z')
                 guess[j] += 32;
-            if(( !j || guess [j-1] == ' ' ) && guess [j] == acr [c] )
+            if ((!j || guess [j-1] == ' ') && guess [j] == acr [c])
             {
             	c++;
                	guess [j] -= 32;
             }
         }
-        printf( "%s\n", c == acrlen ? guess : "No :(" );
+        printf ("%s\n", c == acrlen ? guess : "No :(");
     }
     return 0;
 }
