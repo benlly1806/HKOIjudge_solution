dca3e7aece27bb923e7aa3f17ff66f69fab4c1c8
licorimon <pop2lemon@gmail.com>
2017-02-18 23:00:12 +0800

    Update M1602 Valentine Roses.c

--- M1602 Valentine Roses.c
+++ M1602 Valentine Roses.c
@@ -1,19 +1,19 @@
 #include<stdio.h>
 
-int main()
+int main ()
 {
-    int fin[25]={1,1,1,1,1,
-                 1,2,2,2,1,
-                 1,2,3,2,1,
-                 1,2,2,2,1,
-                 1,1,1,1,1};
-    int i,input[25],count=0;
-    for(i=0;i<26;i++)
+    int fin [25] = {1, 1, 1, 1, 1,
+                    1, 2, 2, 2, 1,
+                    1, 2, 3, 2, 1,
+                    1, 2, 2, 2, 1,
+                    1, 1, 1, 1, 1};
+    int i, input [25], count = 0;
+    for (i = 0 ; i < 26 ; i++)
     {
-        scanf("%d",&input[i]);
-        if(input[i]!=fin[i])
+        scanf ("%d", &input [i]);
+        if (input [i] != fin [i])
             count++;
     }
-    printf("%d\n",count/2);
+    printf ("%d\n", count / 2);
     return 0;
 }
