edfe45e12d2ae3419ff876901916b70a9fa67f33
licorimon <pop2lemon@gmail.com>
2017-02-18 23:07:20 +0800

    Update J083 Count 1s.c

--- J083 Count 1s.c
+++ J083 Count 1s.c
@@ -1,17 +1,17 @@
 #include<stdio.h>
 #include<math.h>
 
-int main()
+int main ()
 {
-    long long int num,ans=0;
+    long long int num, ans = 0;
     int i;
-    scanf("%lld",&num);
-    for(i=0;i<=58;i++)
+    scanf ("%lld", &num);
+    for (i = 0 ; i <= 58 ; i++)
     {
-        ans+=(num/((long long int)(pow(2,i+1))))*((long long int)(pow(2,i)));
-        if(num%((long long int)pow(2,i+1))>=((long long int)pow(2,i)))
-            ans+=(num%((long long int)(pow(2,i))))+1;
+        ans += (num / ((long long int) (pow (2, i + 1)))) * ((long long int) (pow (2, i)));
+        if (num % ((long long int) pow(2, i + 1)) >= ((long long int) pow(2, i)))
+            ans += (num % ((long long int) (pow(2, i)))) + 1;
     }
-    printf("%lld\n",ans);
+    printf ("%lld\n", ans);
     return 0;
 }
