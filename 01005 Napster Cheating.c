f9f7a7c9b532ed105f08107c89d180a0f131d798
licorimon <pop2lemon@gmail.com>
2017-02-18 23:23:26 +0800

    Update 01005 Napster Cheating.c

--- 01005 Napster Cheating.c
+++ 01005 Napster Cheating.c
@@ -1,19 +1,19 @@
 #include<stdio.h>
 #include<string.h>
 
-int main()
+int main ()
 {
-	char name[1000],tmp;
-	int i,j,len;
-	gets(name);
-	len=strlen(name);
-	for(j=0;j<len;j++)
-		for(i=0;i<len-5-j;i++)
+	char name [1000],tmp;
+	int i, j, len;
+	gets (name);
+	len = strlen (name);
+	for (j = 0 ; j < len ; j++)
+		for (i = 0 ; i < len - 5 - j ; i++)
 		{
-			tmp=name[i];
-			name[i]=name[i+1];
-			name[i+1]=tmp;
+			tmp = name [i];
+			name [i] = name [i + 1];
+			name [i + 1] = tmp;
 		}
-	printf("%s\n",name);
+	printf ("%s\n", name);
 	return 0;
 }
