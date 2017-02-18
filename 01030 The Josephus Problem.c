e32763760222e5b4643c63ceddb710b52ce82158
licorimon <pop2lemon@gmail.com>
2017-02-18 23:08:54 +0800

    Update 01030 The Josephus Problem.c

--- 01030 The Josephus Problem.c
+++ 01030 The Josephus Problem.c
@@ -1,20 +1,20 @@
 #include<stdio.h>
 
-int main()
+int main ()
 {
     int x;
-    scanf("%d",&x);
-    int next[x+1],now=1;
-    int i,j;
-    for(i=1;i<x;i++)
-        next[i]=i+1;
-    next[i]=1;
-    while(x-->1)
+    scanf ("%d", &x);
+    int next [x + 1], now = 1;
+    int i, j;
+    for (i = 1 ; i < x ; i++)
+        next [i] = i + 1;
+    next [i] = 1;
+    while (x-- > 1)
     {
-        printf("%d ",next[now]);
-        next[now]=next[next[now]];
-        now=next[now];
+        printf ("%d ", next [now]);
+        next [now] = next [next [now]];
+        now = next [now];
     }
-    printf("\n%d\n",now);
+    printf ("\n%d\n", now);
     return 0;
 }
