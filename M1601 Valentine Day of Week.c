ef003c785b7f8d5759b5e947e665d4ac4bd9a260
licorimon <pop2lemon@gmail.com>
2017-02-18 23:01:35 +0800

    Update M1601 Valentine Day of Week.c

--- M1601 Valentine Day of Week.c
+++ M1601 Valentine Day of Week.c
@@ -1,13 +1,13 @@
 #include<stdio.h>
 
-int main()
+int main ()
 {
-    int i,yr,day=0;
-    char ans[7][11]={"Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"};
-    scanf("%d",&yr);
-    for(i=2000;i<yr;i++,day++,day%=7)
-    	if(!(i%400)||(!(i%4)&&(i%100)))
+    int i, yr, day = 0;
+    char ans [7] [11] = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
+    scanf ("%d", &yr);
+    for (i = 2000 ; i < yr ; i++ , day++ , day %= 7)
+    	if (! (i % 400) || (! (i % 4) && (i % 100)))
             day++;
-    printf("%s\n",ans[day]);
+    printf ("%s\n", ans  [day]);
     return 0;
 }
