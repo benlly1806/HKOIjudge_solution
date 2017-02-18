8a595990b8a66f0a5a75cb9de91f79cf6a28a11b
licorimon <pop2lemon@gmail.com>
2017-02-18 22:58:36 +0800

    Update S140 Enumeration (2014 Senior).c

--- S140 Enumeration (2014 Senior).c
+++ S140 Enumeration (2014 Senior).c
@@ -2,10 +2,10 @@
 
 int main()
 {
-    int input,i,j,c;
-    scanf("%d",&input);
-    for(i=input;i>0;i--)
-        for(j=1;j<=input;j++)
-            printf("%d%c",j==1?(i==input?c=input*input*(input+1)/2:(c-=i+1)):(c-=i),j==input?'\n':' ');
+    int input, i, j, c;
+    scanf ("%d", &input);
+    for (i = input ; i > 0 ; i--)
+        for (j = 1 ; j <= input ; j++)
+            printf ("%d%c", j == 1 ? (i == input ? c = input * input * ( input + 1 ) / 2 : ( c-= i + 1 ) ) : (c -= i ) , j == input ? '\n' : ' ');
     return 0;
 }
